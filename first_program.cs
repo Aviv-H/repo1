d1d818869c2a1781263c73617d55ae49a2c45070
Aviv <aviv144h@gmail.com>
2023-09-09 15:43:17 +0300

    modified Hello_World.cs, added first_program.cs

--- first_program.cs
+++ first_program.cs
@@ -4,7 +4,6 @@ namespace HelloWorld
     class Hello {         
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
 			System.Console.WriteLine("This is the fisrt program");
         }
     }
